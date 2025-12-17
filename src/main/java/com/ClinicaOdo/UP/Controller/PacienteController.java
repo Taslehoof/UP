f9bb5302cbb72022726a1b7635e5670d282b57ed
Sergio Conti <seracon@gmail.com>
2025-12-17 07:19:40 -0300

    Modificaciones menores
    Modificaciones menores en algunas clases

---
+++ src/main/java/com/ClinicaOdo/UP/Controller/PacienteController.java
@@ -0,0 +1,17 @@
+package com.ClinicaOdo.UP.Controller;
+
+import com.ClinicaOdo.UP.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+    private PacienteService pacienteService;
+
+    @Autowired
+    public PacienteController(PacienteService pacienteService){
+        this.pacienteService = pacienteService;
+    }
+}
