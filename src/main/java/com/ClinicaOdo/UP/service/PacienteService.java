e7ae68ffeda37ea66ab6525eb2cb4eea906eee6c
Sergio Conti <seracon@gmail.com>
2025-12-22 15:06:20 -0300

    Mínimas modificaciones
    Mínimas modificaciones en algunas clases para agregar funciones nuevas.

--- src/main/java/com/ClinicaOdo/UP/service/PacienteService.java
+++ src/main/java/com/ClinicaOdo/UP/service/PacienteService.java
@@ -1,4 +1,36 @@
 package com.ClinicaOdo.UP.service;
 
+import com.ClinicaOdo.UP.entity.Paciente;
+import com.ClinicaOdo.UP.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public Paciente guardarPaciente(Paciente paciente) {
+        return pacienteRepository.save(paciente);
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return pacienteRepository.findById(id);
+    }
+
+    public Optional<Paciente> buscarPorEmail(String email) {
+        return pacienteRepository.findByEmail(email);
+    }
+
+    public List<Paciente> buscarTodosLosPacientes() {
+        return pacienteRepository.findAll();
+    }
+
+    public void borrarPaciente(Long id) {
+        pacienteRepository.deleteById(id);
+    }
 }
