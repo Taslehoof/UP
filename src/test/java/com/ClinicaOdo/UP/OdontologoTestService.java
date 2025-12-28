9783bd6023c4108263e8899fb9a93f175ad19cd0
Sergio Conti <seracon@gmail.com>
2025-12-28 11:56:06 -0300

    Se agrego clase de Odontologo en Test Unit
    Se agrego clase de Odontolgo en Test y se cumplimentan algunas funciones.

---
+++ src/test/java/com/ClinicaOdo/UP/OdontologoTestService.java
@@ -0,0 +1,52 @@
+package com.ClinicaOdo.UP;
+
+import com.ClinicaOdo.UP.entity.Odontologo;
+import com.ClinicaOdo.UP.service.OdontologoService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class OdontologoTestService {
+    @Autowired
+    private OdontologoService odontologoService;
+
+    private Odontologo crearOdontologoTestUnitario(){
+
+       Odontologo odontologo = new Odontologo();
+
+       odontologo.setNombre("Andres");
+       odontologo.setApellido("Serrano");
+       odontologo.setMatricula("FakeTu1234");
+
+       return odontologo;
+    }
+
+   @Test
+   void guardarOdontologo_deberiaPersisteYRetorna(){
+        // DADO
+       System.out.println("Probando guardar un Odontologo");
+       Odontologo odontologo = crearOdontologoTestUnitario();
+
+       // CUANDO
+       Odontologo odontologoGuardado = odontologoService.guardarOdontologo(odontologo);
+
+       // ENTONCES
+       assertNotNull(odontologoGuardado.getId(),"Debe retornar un ID luego de guardar");
+       Optional<Odontologo> buscado = odontologoService.buscarPorId(odontologoGuardado.getId());
+       assertTrue(buscado.isPresent(),"El odontologo debe existir");
+       assertEquals(odontologoGuardado.getId(),buscado.get().getId(), "El id del Odontolgo es Incorrecto");
+       assertEquals(odontologoGuardado.getNombre(), buscado.get().getNombre(),"El nombre del odontolgoo guardado es incorrecto");
+   }
+
+   @Test
+   void borrarOdontologo_DeberiaEliminarOdontologo(){
+
+   }
+}
